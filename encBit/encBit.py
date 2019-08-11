d79a63edce66e85559b2817733c563ea11e70a25
programmer-666 <suharslan@protonmail.ch>
2019-08-11 21:24:55 +0300

    Düzenlemeler

--- encBit/encBit.py
+++ encBit/encBit.py
@@ -16,9 +16,14 @@ def AsciiToText(AscLst):
     for i in AscLst:
         lst.append(chr(i))
     return lst
-def WriteAFile(Fname,format,content):
+def WriteAFile(fname,format,content):
     File = open(fname+"."+format,"w")
     for i in content:
         File.write(i)
     File.close()
 #functions
+Ascii = TextToAscii(text)
+CrAscii = xor(Ascii,ky)
+Hash = AsciiToText(CrAscii)
+print(Hash)
+WriteAFile("encF","txt",Hash)
