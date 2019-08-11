4bef288bf7825360dd1ecfc6428e61cc69dd20c9
programmer-666 <suharslan@protonmail.ch>
2019-08-11 21:10:34 +0300

    Fonksiyonlar eklendi

--- encBit/encBit.py
+++ encBit/encBit.py
@@ -1,29 +1,24 @@
 text = input("Enter Text : ")
 ky = abs(int(input("Enter Key (Max255Min1) : ")))
-bitler = []
-hash = []
-Openhsh = []
-encFile = open("encF.txt","w")
 #variable
-""" Islemler
-if ky <=255:
-    for i in text.upper():
-        bitler.append(ord(i))
-    for j in bitler:
-        hash.append(j^ky)
-
-    for k in hash:
-        Openhsh.append(chr(k))
-        print("Hash : {}  ,  {}".format(Openhsh,ha  sh))
-    for l in Openhsh:
-        encFile.write(str(l))
-    encFile.close()
-else:
-    print("Key too long")
-"""
-#functions
 def TextToAscii(Text):
     lst = []
     for i in Text.upper():
         lst.append(ord(i))
     return lst
+def xor(BitList,Key):
+    lst = []
+    for i in BitList:
+        lst.append(i^Key)
+    return lst
+def AsciiToText(AscLst):
+    lst = []
+    for i in AscLst:
+        lst.append(chr(i))
+    return lst
+def WriteAFile(Fname,format,content):
+    File = open(fname+"."+format,"w")
+    for i in content:
+        File.write(i)
+    File.close()
+#functions
