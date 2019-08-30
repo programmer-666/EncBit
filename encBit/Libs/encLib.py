23bd23558f6695ce3822822a174371a41c917ce6
programmer-666 <suharslan@protonmail.ch>
2019-08-30 22:17:48 +0300

    lib changed

--- encBit/Libs/encLib.py
+++ encBit/Libs/encLib.py
@@ -1,20 +1,27 @@
-def TextToAscii(Text):
-    lst = []
-    for i in Text.upper():
-        lst.append(ord(i))
-    return lst
-def encryption(BitList,Key):
-    lst = []
-    for i in BitList:
-        lst.append(i^Key)
-    return lst
-def AsciiToText(AscLst):
-    lst = []
-    for i in AscLst:
-        lst.append(chr(i))
-    return lst
-def WriteAFile(fname,format,content):
-    File = open(fname+"."+format,"w")
-    for i in content:
-        File.write(i)
-    File.close()
+class eLib():
+    def __init__(self,key):
+        self.key = key
+
+    def TextToAscii(self,Text):
+        lst = []
+        for i in Text.upper():
+            lst.append(ord(i))
+        return lst
+
+    def encryption(self,BitList):
+        lst = []
+        for i in BitList:
+            lst.append(i^self.key)
+        return lst
+
+    def AsciiToText(self,AscLst):
+        lst = []
+        for i in AscLst:
+            lst.append(chr(i))
+        return lst
+
+    def WriteAFile(self,fname,format,content):
+        File = open(fname+"."+format,"w")
+        for i in content:
+            File.write(i)
+        File.close()
